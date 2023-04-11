c2760775ea1639addc3e4030890c7c45cea9ee70
Bishr Tabbaa <bishrt@amazon.com>
2023-04-11 01:51:35 +0000

    Refactored 01_SageMakerProcessing.rmd script to reuse 00_RPythonSetup.sh script in bash block per seanpmorgan

--- 02_AdvancedSageMaker/00_RPythonSetup.sh
+++ 02_AdvancedSageMaker/00_RPythonSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Reinstall numpy to workaround conflict between reticulate and RStudio v4.x due to BLAS library dependency
-# The reinstall process takes 2 minutes.
+# The reinstall process takes 5 minutes.
 # https://github.com/rstudio/reticulate/issues/1257
 
 python_path=$(which python)
