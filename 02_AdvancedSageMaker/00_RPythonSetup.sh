d9e9617901cc4751a491b951d9fde04580ad1bf3
Bishr Tabbaa <bishrt@amazon.com>
2023-04-14 20:28:39 +0000

    Remove external sh file.. used bash embedded into R.rmd markdown files.

--- 02_AdvancedSageMaker/00_RPythonSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Reinstall numpy to workaround conflict between reticulate and RStudio v4.x due to BLAS library dependency
-# The reinstall process takes 5 minutes.
-# https://github.com/rstudio/reticulate/issues/1257
-
-python_path=$(which python)
-echo $python_path
-sudo $python_path -m pip install --no-user --force-reinstall --no-binary numpy numpy
